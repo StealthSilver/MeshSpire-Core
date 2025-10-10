f803e8006d559e75cc2e17f10a46667c692f5546
komal-kumawat <kumawatkomal17122004@gmail.com>
2025-10-10 15:53:44 +0530

    profile done

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://meshspire-core-vjqd.onrender.com/api/v0",
+  baseURL: "https://meshspire-core-vjqd.onrender.com/api/v0",
   // baseURL:"http://localhost:8000/api/v0",
-  baseURL:"https://meshspire.onrender.com/api/v0",
+  // baseURL:"https://meshspire.onrender.com/api/v0",
   headers: {
     "Content-Type": "application/json",
   },
